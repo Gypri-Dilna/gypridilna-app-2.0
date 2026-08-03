d35c48a22c5979f5ab3d2b0f11b15374143b834a
dilna-netizen <dilna@gypri.cz>
2026-08-03 17:31:19 +0200

    refactor: update RFID Access Control Dashboard application structure

--- OLD Apps + sources/Old gypridilna app + inventory system/app/proguard-rules.pro
+++
@@ -1,1 +0,0 @@
-# Proguard rules are disabled for this project to ensure stability.
--- OLD Apps + sources/Old gypridilna app + inventory system/app/src/main/java/cz/gypridilna/inventarizace/data/network/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package cz.gypridilna.inventarizace.data.network
-
-import cz.gypridilna.inventarizace.data.entities.InventoryItem
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("exec")
-    suspend fun getInventory(): List<InventoryItem>
-}
